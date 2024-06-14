5792d16a0b3bbe3f6ed8c53c4faae72f1312ff6b
patricio <p.carvajal@duocuc.cl>
2024-06-14 11:24:18 -0400

    opcion_1_lista_sin_validar

--- 1.py
+++ 1.py
@@ -1,5 +1,8 @@
 import os, time, msvcrt
 from funciones1 import *
+
+trabajadores = []
+
 while True:
     print("""
     >MENU TRABAJADORES<
@@ -10,13 +13,23 @@ while True:
 
     opc = int(input('ingrese opción:'))
     if opc == 1:
-        pass
-        limpiar_pantalla()
+        print("registrar trabajador")
+        nombre_apellido = input("ingrese nombre y su apellido: ")
+        cargo = int(input("ingrese su cargo (1.CEO, 2.DESARROLLADOR, 3:ANALISTA)"))
+        sueldo_b = int(input("ingrese sueldo bruto: "))
+        desc_salud = round(7/100* sueldo_b)
+        desc_afp  = int(12/100*sueldo_b)
+        sueldo_liquido = sueldo_b-desc_afp-desc_salud
+        trabajador = [nombre_apellido, cargo, sueldo_b, desc_salud, desc_afp,sueldo_liquido]
+        trabajadores.append(trabajador)
+        print('Trabajador registrado con exito!!')
+        esperar_t()
+        limpiar_p()
     elif opc == 2:
         pass
-        limpiar_pantalla()
+        esperar_t()
     elif opc == 3:
         pass
-        limpiar_pantalla()
+        esperar_t()
     else:
         break
--- funciones1.py
+++ funciones1.py
@@ -2,3 +2,12 @@ import os, msvcrt, time
 
 def limpiar_pantalla():
     time.sleep(3)
+
+def esperar_t():
+    print('>>>Presione tecla para continuar<<<')
+    msvcrt.getch()
+
+def limpiar_p():
+    os.system
+
+#################################################################################################
